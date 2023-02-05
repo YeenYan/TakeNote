a0fd36624577b656b779407cb1ed50e6bf095963
YeenYan <markiantalan@gmail.com>
2023-02-05 19:00:50 +0800

    setup/configure vue files, tailwind & postcss

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1 class="sample">HEllo world</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style lang="postcss" scoped>
+.container {
+  @apply bg-neutral-800;
+}
+.sample {
+  @apply text-9xl text-red-500;
+}
+</style>
